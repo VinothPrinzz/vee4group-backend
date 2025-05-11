032cab3ca69cd33f8cdad86093aaec3b74cd9097
vinoth m <120578231+VinothPrinzz@users.noreply.github.com>
2025-05-11 18:08:03 +0530

    Update cors

--- server/index.js
+++ server/index.js
@@ -85,7 +85,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://vee4group-frontend.onrender.com', 
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
